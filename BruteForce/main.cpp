b6914171fb18d3bbf5b634ab8de5a2f9d0e48317
Ethan "Yeung Ethan Ethan Yeung Yeung Ethan" Yeung <yeungmethan@gmail.com>
2024-09-30 15:07:09 -0400

    Add files for brute force and linear programming algorithm

---
+++ BruteForce/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// Include headers from data structure
+
+/* 4 For Loops
+	1. Iterate weeks
+	2. Iterate days per week
+	3. Iterate nurses
+	4. Iterate shifts
+	5. ...
+	*/
+int main(){
+
+}
