1aa0a8b5c764e6c6a824f093b407a8b412a1d40f
brianpotteiger <brianpott15@gmail.com>
2024-10-22 11:51:23 -0400

    Added dynamic department headers

--- UI/JS/DisplayFunctionality.js
+++ UI/JS/DisplayFunctionality.js
@@ -3,13 +3,34 @@ let currentTab = "Overall";
     Need some kind of function that reads in the JSON file (or whatever other input) and makes list of all departments
     Can store list of departments as array then change tabs and tabSelected's to elements of that array
 */
+let arrDepartments = ["Overall", "Oncology", "Pediatric", "Surgery"];
+function fillTabs () {
+    console.log(arrDepartments);
+    for (let department of arrDepartments) {
+        console.log('Inserting tab:' + department);
+        //Fill tab header with all departments
+        document.getElementById("tabs").innerHTML += `<button onClick="switchTab('${department}')">${department}</button>`;
+    }
+    
+}
 function switchTab (tabSelected) {
     //Change header at top of page
     currentTab = tabSelected;
-    console.log(tabSelected);
     document.getElementById('tabHeader').innerHTML = `${currentTab} Schedule`;
     //Switch table
-    if (tabSelected == 'Pediatric') {
+    for (let department of arrDepartments) {
+        let oldTabID = department.toLowerCase();
+        oldTabID = oldTabID.concat("Schedule");
+        let oldTab = document.getElementById(oldTabID);
+        oldTab.setAttribute("hidden", "hidden");
+        console.log('Hiding tab: ' + oldTabID);
+    }
+    let selectedTabID = tabSelected.toLowerCase();
+    selectedTabID = selectedTabID.concat("Schedule"); 
+    console.log('Switching to ' + selectedTabID);
+    let selectedTab = document.getElementById(selectedTabID);
+    selectedTab.removeAttribute("hidden");
+    /*if (tabSelected == 'Pediatric') {
         console.log('Switching to pediatric')
         let selectedTab = document.getElementById('pediatricSchedule');
         let oldTab = document.getElementById('oncologySchedule');
@@ -25,5 +46,5 @@ function switchTab (tabSelected) {
     }
     else {
         console.log('Switching to overall')
-    }
+    }*/
 }
