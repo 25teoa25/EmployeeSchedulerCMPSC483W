f105a07150b0e4898b6aec9289d936c018f5b968
Bogdan Pricop <bvp5361@psu.edu>
2024-11-21 22:22:40 -0500

    Update GeneticAlgo.h

--- DataStructure/LinkedListDS/GeneticAlgo.h
+++ DataStructure/LinkedListDS/GeneticAlgo.h
@@ -1,1 +1,9 @@
+#ifndef GENETIC_ALGORITHM_H
+#define GENETIC_ALGORITHM_H
 
+#include "NurseList.h" // For ShiftSchedule
+#include <random>
+
+ShiftSchedule geneticAlgorithm();
+
+#endif // GENETIC_ALGORITHM_H
